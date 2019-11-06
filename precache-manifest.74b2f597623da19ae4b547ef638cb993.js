5ab04a7bad8ab978985cbe1ed52d1addfca5fcad
Sijey <Popov040391@gmail.com>
2019-11-06 19:15:07 +0200

    Updates

---
+++ precache-manifest.74b2f597623da19ae4b547ef638cb993.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3aa008d0ed673a2848f8a5af61cfda",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "843fa62b5fc7a8fbec7b",
+    "url": "/react_people-table/static/js/2.021c71be.chunk.js"
+  },
+  {
+    "revision": "82a4cfe66c96e6bf52b3",
+    "url": "/react_people-table/static/js/main.cef2ee63.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
